902b5adbd9e4c0be76327d554cb2d04387b83e7a
Pizatol <dev.belmonte@gmail.com>
2022-03-25 16:02:03 +0100

    fin responsive

--- pages/Methode.js
+++ pages/Methode.js
@@ -1,39 +1,38 @@
 import React from "react";
 import css from "../styles/Methode.module.scss";
 
-
 import Image from "next/image";
-import book from '../public/assets/methode/book.jpg'
-import cook from '../public/assets/methode/cook.jpg'
-import tea from '../public/assets/methode/tea.jpg'
+import book from "../public/assets/methode/book.jpg";
+import cook from "../public/assets/methode/cook.jpg";
+import tea from "../public/assets/methode/tea.jpg";
 
 export default function Methode() {
     return (
         <>
             <div className={css.globalContainer}>
                 <h1>Méthode personnelle</h1>
-					 <div className={css.delimitation}></div>
+                <div className={css.delimitation}></div>
                 <div className={css.firstCard}>
-					 <div className={css.text}>
+                    <div className={css.text}>
                         <p>
                             Je choisis un manuel (adapté aux élèves) pour
-                            avancer dans les cours en groupe et semi-individuels, ce qui leur permettra après le cours, de
-                            réviser seuls chez eux. En m&apos;appuyant sur le
-                            livre, j&apos;introduits de multiples jeux et je
-                            crée des conversations pour que les élèves puissent
-                            participer.{" "}
+                            avancer dans les cours en groupe et
+                            semi-individuels, ce qui leur permettra après le
+                            cours, de réviser seuls chez eux. En m&apos;appuyant
+                            sur le livre, j&apos;introduits de multiples jeux et
+                            je crée des conversations pour que les élèves
+                            puissent participer.{" "}
                         </p>
                     </div>
                     <div className={css.img}>
-							  <Image
-								  src={book}
-								  width={3264 }
-								  height={2448 }
-								  alt='image livre japonais'
-								  layout="responsive"
-							  />
-						  </div>
-                    
+                        <Image
+                            src={book}
+                            width={3264}
+                            height={2448}
+                            alt="image livre japonais"
+                            layout="responsive"
+                        />
+                    </div>
                 </div>
                 <p>
                     J&apos;organise des sorties pour faire découvrir la culture
@@ -46,29 +45,26 @@ export default function Methode() {
                         <li>...</li>
                     </ul>
                 </p>
-					 <div className={css.bottomCard}>
-					 	<div>
-
-					 <Image
-						 src={tea}
-						 width={1920}
-						 height={1282}
-						 alt="cérémonie du thé"
-						 layout="responsive"
-					 />
-						 </div>
-						 <div>
-
-					 <Image
-						 src={cook}
-						 width={1920}
-						 height={1282}
-						 alt="cuisine"
-						 layout="responsive"
-					 />
-						 </div>
-
-					 </div>
+                <div className={css.bottomCard}>
+                    <div>
+                        <Image
+                            src={tea}
+                            width={1920}
+                            height={1282}
+                            alt="cérémonie du thé"
+                            layout="responsive"
+                        />
+                    </div>
+                    <div>
+                        <Image
+                            src={cook}
+                            width={1920}
+                            height={1282}
+                            alt="cuisine"
+                            layout="responsive"
+                        />
+                    </div>
+                </div>
             </div>
         </>
     );
